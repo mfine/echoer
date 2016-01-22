a311d7f5c34ed55a1aba93a683a0d1ffa80094ce
Mark Fine <mark.fine@gmail.com>
2016-01-21 18:05:19 -0800

    options.

--- echoer-data.hs
+++
@@ -1,9 +0,0 @@
-import Conduit
-import Data.Conduit.Network
-
-main :: IO ()
-main =
-  runTCPServer (serverSettings 5000 "*") $ \appData ->
-    runResourceT $
-      appSource appData $$
-      sinkFile "data"
--- echoer.hs
+++ echoer.hs
@@ -1,10 +1,44 @@
 import Conduit
 import Data.Conduit.Network
-import System.IO
+import Data.Time
+import Options.Applicative
+
+data Conf = Conf
+  { confFile :: String
+  , confPort :: Int
+  } deriving ( Eq, Show )
+
+file :: Parser String
+file =
+  strOption
+    $  long    "file"
+    <> short   'f'
+    <> metavar "FILE"
+    <> value   "data"
+    <> help    "File prefix to write"
+
+port :: Parser Int
+port =
+  option auto
+    $  long    "port"
+    <> short   'p'
+    <> metavar "PORT"
+    <> value   5000
+    <> help    "Port to listen on"
+
+parseConf :: Parser Conf
+parseConf = Conf <$> file <*> port
+
+parser :: Parser a -> ParserInfo a
+parser parse = info ( helper <*> parse ) fullDesc
 
 main :: IO ()
-main =
-  runTCPServer (serverSettings 5000 "*") $ \appData ->
+main = do
+  c <- execParser $ parser parseConf
+  t <- getCurrentTime
+  runTCPServer (serverSettings (confPort c) "*") $ \appData ->
     runResourceT $
       appSource appData $$
-      sinkHandle stdout
+      sinkFile (confFile c ++ "-" ++ timestamp t) where
+        timestamp = formatTime defaultTimeLocale "%Y%m%d-%H%M%S"
+
